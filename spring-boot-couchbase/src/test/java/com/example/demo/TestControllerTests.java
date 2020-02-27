ab58881663617fa249140b73ad556eaa09366321
namgunghyeon <onekyosi@gmail.com>
2020-02-27 21:15:01 +0900

    feat: spring-boot-couchbase rest docs 적용중

---
+++ spring-boot-couchbase/src/test/java/com/example/demo/TestControllerTests.java
@@ -0,0 +1,84 @@
+package com.example.demo;
+
+import com.example.demo.controller.TestController;
+import com.example.demo.service.TestService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static com.example.demo.ApiDocumentUtils.getDocumentRequest;
+import static com.example.demo.ApiDocumentUtils.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.beneathPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+
+
+@WebMvcTest(TestController.class)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com") // (1)
+public class TestControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean // (2)
+    private TestService testService;
+
+    @Test
+    public void test() throws Exception {
+        List<com.example.demo.model.Test> list = new ArrayList<>();
+        com.example.demo.model.Test data = new com.example.demo.model.Test();
+        data.setId("id");
+        data.setField("field");
+        data.setValue("value");
+        list.add(data);
+
+        given(testService.getTest(any()))
+                .willReturn(list);
+
+        ResultActions result = this.mockMvc.perform(
+                get("/test")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        System.out.println(result.toString());
+        result.andExpect(status().isOk())
+                .andDo(document("test",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("[].id").type(JsonFieldType.STRING).description("id"),
+                                fieldWithPath("[].value").type(JsonFieldType.STRING).description("value"),
+                                fieldWithPath("[].field").type(JsonFieldType.STRING).description("field")
+
+                        )
+                ));
+
+    }
+}
