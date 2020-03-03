878a5bc8ac732e3d73e415eeed90775d9a523c10
namgunghyeon <onekyosi@gmail.com>
2020-03-03 21:15:21 +0900

    fix:  spring-boot-rest-api-exception NoHandlerFoundException 추가

--- spring-boot-rest-api-exception/src/main/java/com/example/demo/exception/RestResponseEntityExceptionHandler.java
+++ spring-boot-rest-api-exception/src/main/java/com/example/demo/exception/RestResponseEntityExceptionHandler.java
@@ -6,20 +6,26 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableWebMvc
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler {
-    @ExceptionHandler(value = {UnrecognizedPropertyException.class})
+    @ExceptionHandler({UnrecognizedPropertyException.class})
     protected ResponseEntity<Object> handleUnrecognizedPropertyException(UnrecognizedPropertyException ex) {
         List<String> errors = new ArrayList<>();
 
@@ -32,5 +38,10 @@ public class RestResponseEntityExceptionHandler {
     @ExceptionHandler({ ResourceNotFoundException.class })
     protected ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return new ResponseEntity<>(new ApiErrorResponse(HttpStatus.NOT_FOUND, "Not found"), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({NoHandlerFoundException.class})
+    protected ResponseEntity<Object> handleResourceNotFoundException(NoHandlerFoundException ex) {
+        return new ResponseEntity<>(new ApiErrorResponse(HttpStatus.NOT_FOUND, "Not found url"), HttpStatus.NOT_FOUND);
     }
 }
